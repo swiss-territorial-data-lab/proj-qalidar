4e8a50a8ea576ca4d5bd97d6520547e7fbf5e181
nmuenger <m.m.nicolas234@gmail.com>
2024-02-07 10:50:47 +0000

    colors

--- scripts/constant.py
+++ scripts/constant.py
@@ -6,3 +6,14 @@ NOISE = 7
 WATER = 9
 BRIDGE = 17
 
+# Taken from https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
+class BColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
